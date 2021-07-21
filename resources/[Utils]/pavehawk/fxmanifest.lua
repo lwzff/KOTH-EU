1997c39036629adaf85f531e633bddbcfad2590e
LozeKylian <kylian4509@hotmail.fr>
2021-07-21 15:35:12 +0200

    Init files

---
+++ resources/[Utils]/pavehawk/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'bodacious'
+
+game 'gta5'
+ 
+files {
+    'handling.meta',
+    'vehicles.meta',
+    'carvariations.meta',
+    'carcols.meta',
+}
+
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+-- data_file 'CARCOLS_FILE' 'carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
+
